0ee1a82ad98a9b62f45a15a700662269f10c08aa
chilln <kimjihan77@naver.com>
2024-05-19 11:16:24 +0900

    Fix:delete prefix, modify tags, description

--- app/main.py
+++ app/main.py
@@ -4,12 +4,14 @@ from app.api.v1.gateway import router as v1_router
 app = FastAPI(
     title="{서비스이름} gateway 서버",
     description="""
-    {서비스이름}의 게이트웨이 서버.
+    서비스 서버
+        1. user (회원)
+        2. invitation (초대)
 
-    아래와 같은 기능을 수행하는 서버입니다
-    1. 인증 및 인가
-    2. 라우팅 
+    아래와 같은 기능을 수행
+        1. 인증 및 인가
+        2. 각 서비스를 담당하는 서버에 라우팅 
     """
 )
 
-app.include_router(v1_router, prefix="/api/v1", tags=["v1"])
+app.include_router(v1_router, tags=["gateway"])
