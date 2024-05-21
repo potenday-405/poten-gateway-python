c1888ce9f91e30ff000c0f44903ee7c1f4a06f21
chilln <kimjihan77@naver.com>
2024-05-21 14:23:29 +0900

    Feat:create envicornment variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,16 @@
-# 인증인가
+import os
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+# .env 파일을 로드
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+TEST_DB_USER = os.getenv("TEST_DB_USER")
+TEST_DB_PASSWORD = os.getenv("TEST_DB_PASSWORD")
+TEST_DB_HOST = os.getenv("TEST_DB_HOST")
+TEST_DB_PORT = 3306
+TEST_DB_BASE = os.getenv("TEST_DB_BASE")
