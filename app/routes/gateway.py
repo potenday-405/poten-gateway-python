e10a07d04d0d93b6cd634f7b58c538a8167e750c
chilln <kimjihan77@naver.com>
2024-05-22 15:52:36 +0900

    Fix:delete DB connection

--- app/routes/gateway.py
+++ app/routes/gateway.py
@@ -15,21 +15,22 @@ router = APIRouter()
 SERVICES = {
     # 서버용
     # "user" : "10.0.8.7:8000/user", 
+    # "profile" : "10.0.8.7:8000/profile", 
     # 로컬용
-    # "user" : "http://127.0.0.1:8080/user",
+    "user" : "http://127.0.0.1:8080/user",
     # 걍진짜 테스트
-    "user" : "http://175.45.203.113:8000/user",
+    # "user" : "http://175.45.203.113:8000/user",
     "invitation" : "10.0.5.6:8080"
 }
 
 @router.get("/{service}/{path:path}")
-async def get_proxy_request(service:str, path:str, request:Request, version: str = Header("1.0"), access_token: Annotated[str | None, Header()] = None, db:Session = Depends(get_test_db)):
+async def get_proxy_request(service:str, path:str, request:Request, version: str = Header("1.0"), access_token: Annotated[str | None, Header()] = None):
 
     if not access_token :
         raise HTTPException(status_code=400, detail="No access_token in Header")
 
     else:
-        auth = AuthService(db)
+        auth = AuthService()
         url = f"{SERVICES[service]}/{path}"
 
         payload = await auth.verify_and_create_token(
@@ -41,27 +42,25 @@ async def get_proxy_request(service:str, path:str, request:Request, version: str
         )
 
         # payload가 있는 경우,
-        email = payload.get("sub")
+        
+        user_id = payload.get("sub")
 
-        if email:
-            # user_id값 보내기
-            user_id = await auth.get_user_id(email)
-            async with httpx.AsyncClient() as client:
-                response = await client.get(url, params=request.query_params, headers={"version" : version, "user_id" : str(user_id)})
-                return response.json()
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, params=request.query_params, headers={"version" : version, "user_id" : str(user_id)})
+            return response.json()
         
 
 @router.post("/{service}/{path:path}")
-async def post_proxy_request(service:str, path:str, request:Request, version: str = Header("1.0"), access_token: Annotated[str | None, Header()] = None, db:Session = Depends(get_test_db)):
+async def post_proxy_request(service:str, path:str, request:Request, version: str = Header("1.0"), access_token: Annotated[str | None, Header()] = None):
 
-    auth = AuthService(db)
+    auth = AuthService()
     url = f"{SERVICES[service]}/{path}"
 
     # 로그인 / 회원가입 로직
     if path == "login" or path == "signup":
         async with httpx.AsyncClient() as client:
             response = await client.post(url, content=await request.body(), headers={"version" : version})
-            # return response.json()
+            
             return json.loads(response.content)
 
     # accessToken이 없을 경우, error raise
@@ -78,24 +77,25 @@ async def post_proxy_request(service:str, path:str, request:Request, version: st
             version
         )
 
-        # payload가 있는 경우,
-        email = payload.get("sub")
 
-        if email:
-            # user_id값 보내기
-            user_id = await auth.get_user_id(email)
-            async with httpx.AsyncClient() as client:
-                response = await client.post(url, content=await request.body(), headers={"version" : version, "user_id" : str(user_id)})
-                return response.json()
+        # payload가 있는 경우,
+        user_id = payload.get("sub")
+
+        async with httpx.AsyncClient() as client:
+            
+            response = await client.post(url, content=await request.body(), headers={"version" : version, "user_id" : str(user_id)})
+            return response.json()
+
 
 @router.put("/{service}/{path:path}")
-async def put_proxy_request(service:str, path:str, request:Request, version: str = Header("1.0"), access_token: Annotated[str | None, Header()] = None, db:Session = Depends(get_test_db)):
+async def put_proxy_request(service:str, path:str, request:Request, version: str = Header("1.0"), access_token: Annotated[str | None, Header()] = None):
+    
 
     if not access_token :
         raise HTTPException(status_code=400, detail="No access_token in Header")
 
     else:
-        auth = AuthService(db)
+        auth = AuthService()
         url = f"{SERVICES[service]}/{path}"
 
         payload = await auth.verify_and_create_token(
@@ -107,14 +107,11 @@ async def put_proxy_request(service:str, path:str, request:Request, version: str
         )
 
         # payload가 있는 경우,
-        email = payload.get("sub")
+        user_id = payload.get("sub")
 
-        if email:
-            # user_id값 보내기
-            user_id = await auth.get_user_id(email)
-            async with httpx.AsyncClient() as client:
-                response = await client.put(url, params=await request.body(), headers={"version" : version, "user_id" : str(user_id)})
-                return response.json()
+        async with httpx.AsyncClient() as client:
+            response = await client.put(url, params=await request.body(), headers={"version" : version, "user_id" : str(user_id)})
+            return response.json()
 
 @router.delete("/{service}/{path:path}")
 async def put_proxy_request(service:str, path:str, request:Request, version: str = Header("1.0"), access_token: Annotated[str | None, Header()] = None, db:Session = Depends(get_test_db)):
