268c3113873cb722b5a0e076832bf55a2ae2a124
chilln <kimjihan77@naver.com>
2024-05-30 18:29:43 +0900

    Fix: Refactor forwardAPI Method

--- app/service/auth.py
+++ app/service/auth.py
@@ -1,27 +1,28 @@
 from fastapi import HTTPException, Request
 import jwt
+import json
 import httpx
 from datetime import datetime, timedelta
-from app.core.config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
+from app.core.config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM, USER_URL, INVITATION_URL
 from app.database import models
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 
 SERVICES = {
-    "user" : "10.0.8.7:8000/user",
-    "invitation" : "10.0.5.6:8080"
+    "user" : USER_URL,
+    "invitation" : INVITATION_URL
 }
 
 class UserId(BaseModel):
     user_id : str
 
 class AuthService():
-    def __init__(self):
-        pass
-
+    def __init__(self, header:dict):
+        self.header = header
+        
     @staticmethod
     def get_url(service:str, path:str):
-        return f"http://{SERVICES[service]}/{path}"
+        return f"{SERVICES[service]}/{path}"
 
     @staticmethod
     def create_access_token(self, data: dict, expires_delta: timedelta = None):
@@ -49,33 +50,63 @@ class AuthService():
 
     async def forward_api(
         self, 
+        method:str,
         service : str, 
         path:str,
-        method:str,
         request: Request,
-        token:str,
-        version:str
+        user_id: str | None = None
     ):
+        """
+        서비스 서버로 forwarding 해주는 메소드.
+
+        :params method: API 메소드 종류
+        :params service: API 메소드 종류
+        :params path: API 메소드 종류
+        :params request: API 메소드 종류
+        :params user_id: API 메소드 종류
+
+        :returns : 서비스 서버에서 받아온 response 값
+        """
         if method == "GET":
             async with httpx.AsyncClient() as client:
-                url = get_url(service=service, path=path)
-                response = await client.get(url, params=request.query_params, headers={"version" : version, "access_token" : token})
-                return response.json()
+
+                url = self.get_url(service, path)
+                response = await client.get(url, params=request.query_params, headers={**self.header, "user_id" : str(user_id)})
+
+                if response.status_code != 200:
+                    raise HTTPException(status_code=response.status_code, detail=json.loads(response.content).get("detail"))
+                return json.loads(response.content)
+
         elif method == "POST":
             async with httpx.AsyncClient() as client:
-                url = get_url(service=service, path=path)
-                response = await client.post(url, content=await request.body(), headers={"version" : version})
-                return response.json()
+
+                url = self.get_url(service, path)
+                headers = self.header if not user_id else {**self.header, "user_id" : str(user_id)} 
+
+                response = await client.post(url, content=await request.body(), headers=headers)
+
+                if response.status_code != 200:
+                    raise HTTPException(status_code=response.status_code, detail=json.loads(response.content).get("detail"))
+                return json.loads(response.content)
+
         elif method == "PUT":
             async with httpx.AsyncClient() as client:
-                url = get_url(service=service, path=path)
-                response = await client.put(url, content=await request.body(), headers={"version" : version})
-                return response.json()
+
+                url = self.get_url(service, path)
+                response = await client.put(url, content=await request.body(), params=request.query_params, headers={**self.header, "user_id" : str(user_id), "content-type" : "application/json"})
+
+                if response.status_code != 200:
+                    raise HTTPException(status_code=response.status_code, detail=json.loads(response.content).get("detail"))
+                return json.loads(response.content)
+
         elif method == "DELETE":
             async with httpx.AsyncClient() as client:
-                url = get_url(service=service, path=path)
-                response = await client.delete(url, headers={"version" : version})
-                return response.json()
+                url = self.get_url(service, path)
+                response = await client.delete(url, params=request.query_params, headers={**self.header, "user_id" : str(user_id)})
+                
+                if response.status_code != 200:
+                    raise HTTPException(status_code=response.status_code, detail=json.loads(response.content).get("detail"))
+                return json.loads(response.content)
 
     async def verify_and_create_token(
         self, 
