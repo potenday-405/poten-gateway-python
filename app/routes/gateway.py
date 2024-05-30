aa274b8cc5d54a6f5c1fb71324642196b6dbf88f
chilln <kimjihan77@naver.com>
2024-05-30 19:51:12 +0900

    Fix: refactor router -> repeated logic into a function

--- app/routes/gateway.py
+++ app/routes/gateway.py
@@ -21,122 +21,104 @@ SERVICES = {
 #TODO PROXY_REQUEST 로직 분리하기 -> 중복코드 너무 많음 🚨🚨🚨🚨🚨🚨
 
 @router.get("/{service}/{path:path}")
-async def get_proxy_request(service:str, path:str, request:Request, version: str = Header("1.0"), access_token: Annotated[str | None, Header()] = None):
+async def get_proxy_request(
+    service:str, 
+    path:str, 
+    request:Request, 
+    version: str = Header("1.0"), 
+    access_token: Annotated[str | None, Header()] = None,
+    refresh_token: Annotated[str | None, Header()] = None
+):
+    # AT, RT 둘 다 없는 경우 
+    if not access_token and not refresh_token:
+        raise HTTPException(status_code=400, detail="No token in header")
 
-    if not access_token :
-        raise HTTPException(status_code=400, detail="No access_token in Header")
+    # RT만 있을 경우, 
+    elif not access_token and refresh_token :
+        # refreshToken으로 verify하는 로직.
+        print()
 
+    # AT가 존재할 경우,
     else:
-        auth = AuthService()
-        url = f"{SERVICES[service]}/{path}"
-
-        payload = await auth.verify_and_create_token(
-            service,
-            path,
-            request,
-            access_token,
-            version
-        )
-
-        # payload가 있는 경우,
-        user_id = payload.get("sub")
-
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url, params=request.query_params, headers={"version" : version, "user_id" : str(user_id)})
-            if response.status_code != 200:
-                raise HTTPException(status_code=response.status_code, detail=json.loads(response.content).get("detail"))
-            return json.loads(response.content)
-        
+        auth = AuthService(header={"version" : version})
+        payload = await auth.verify_and_create_token( service, path, request, access_token, version )
+        return await auth.forward_api("GET", service, path, request, payload.get("sub"))
 
 @router.post("/{service}/{path:path}")
-async def post_proxy_request(service:str, path:str, request:Request, version: str = Header("1.0"), access_token: Annotated[str | None, Header()] = None):
+async def post_proxy_request(
+    service:str, 
+    path:str, 
+    request:Request, 
+    version: str = Header("1.0"), 
+    access_token: Annotated[str | None, Header()] = None, 
+    refresh_token: Annotated[str | None, Header()] = None
+):
 
-    auth = AuthService()
-    url = f"{SERVICES[service]}/{path}"
+    auth = AuthService(header={"version" : version})
 
     # 로그인 / 회원가입 로직
     if path == "login" or path == "signup":
-        async with httpx.AsyncClient() as client:
-            response = await client.post(url, content=await request.body(), headers={"version" : version})
-            if response.status_code != 200:
-                raise HTTPException(status_code=response.status_code, detail=json.loads(response.content).get("detail"))
-            return json.loads(response.content)
+        return await auth.forward_api("POST", service, path, request)
 
-    # accessToken이 없을 경우, error raise
-    elif not access_token :
-        raise HTTPException(status_code=400, detail="No access_token in Header")
+    # AT, RT 둘 다 없는 경우 
+    elif not access_token and not refresh_token:
+        raise HTTPException(status_code=400, detail="No token in header")
 
-    # accessToken이 존재할 경우,
-    else:
-        payload = await auth.verify_and_create_token(
-            service,
-            path,
-            request,
-            access_token,
-            version
-        )
+    # RT만 있을 경우, 
+    elif not access_token and refresh_token :
+        # refreshToken으로 verify하는 로직.
+        print()
 
-        # payload가 있는 경우,
-        user_id = payload.get("sub")
-
-        async with httpx.AsyncClient() as client:
-            response = await client.post(url, content=await request.body(), headers={"version" : version, "user_id" : str(user_id), "content-type" : "application/json"})
-            if response.status_code != 200:
-                raise HTTPException(status_code=response.status_code, detail=json.loads(response.content).get("detail"))
-            return json.loads(response.content)
-
+    # AT가 존재할 경우,
+    else :
+        payload = await auth.verify_and_create_token( service, path, request, access_token, version)
+        return await auth.forward_api("POST", service, path, request, payload.get("sub"))
 
 @router.put("/{service}/{path:path}")
-async def put_proxy_request(service:str, path:str, request:Request, version: str = Header("1.0"), access_token: Annotated[str | None, Header()] = None):
+async def put_proxy_request(
+    service:str, 
+    path:str, 
+    request:Request, 
+    version: str = Header("1.0"), 
+    access_token: Annotated[str | None, Header()] = None,
+    refresh_token: Annotated[str | None, Header()] = None
+):
     
+    # AT, RT 둘 다 없는 경우 
+    if not access_token and not refresh_token:
+        raise HTTPException(status_code=400, detail="No token in header")
 
-    if not access_token :
-        raise HTTPException(status_code=400, detail="No access_token in Header")
+    # RT만 있을 경우, 
+    elif not access_token and refresh_token :
+        # refreshToken으로 verify하는 로직.
+        print()
 
+    # AT가 존재할 경우,
     else:
-        auth = AuthService()
-        url = f"{SERVICES[service]}/{path}"
-
-        payload = await auth.verify_and_create_token(
-            service,
-            path,
-            request,
-            access_token,
-            version
-        )
-
-        # payload가 있는 경우,
-        user_id = payload.get("sub")
-
-        async with httpx.AsyncClient() as client:
-            response = await client.put(url, content=await request.body(), params=request.query_params, headers={"version" : version, "user_id" : str(user_id), "content-type" : "application/json"})
-            if response.status_code != 200:
-                raise HTTPException(status_code=response.status_code, detail=json.loads(response.content).get("detail"))
-            return json.loads(response.content)
+        auth = AuthService(header={"version" : version})
+        payload = await auth.verify_and_create_token( service, path, request, access_token, version )
+        return await auth.forward_api("PUT", service, path, request, payload.get("sub"))
 
 @router.delete("/{service}/{path:path}")
-async def put_proxy_request(service:str, path:str, request:Request, version: str = Header("1.0"), access_token: Annotated[str | None, Header()] = None):
+async def put_proxy_request(
+    service:str, 
+    path:str, 
+    request:Request, 
+    version: str = Header("1.0"), 
+    access_token: Annotated[str | None, Header()] = None,
+    refresh_token: Annotated[str | None, Header()] = None
+):
+    # AT, RT 둘 다 없는 경우 
+    if not access_token and not refresh_token:
+        raise HTTPException(status_code=400, detail="No token in header")
 
-    if not access_token :
-        raise HTTPException(status_code=400, detail="No access_token in Header")
-
+    # RT만 있을 경우, 
+    elif not access_token and refresh_token :
+        # refreshToken으로 verify하는 로직.
+        print()
+        
+    # AT가 존재할 경우,
     else:
-        auth = AuthService()
-        url = f"{SERVICES[service]}/{path}"
-
-        payload = await auth.verify_and_create_token(
-            service,
-            path,
-            request,
-            access_token,
-            version
-        )
-
-        # payload가 있는 경우,
-        user_id = payload.get("sub")
-
-        async with httpx.AsyncClient() as client:
-            response = await client.delete(url, params=request.query_params, headers={"version" : version, "user_id" : str(user_id)})
-            if response.status_code != 200:
-                raise HTTPException(status_code=response.status_code, detail=json.loads(response.content).get("detail"))
-            return json.loads(response.content)
+        auth = AuthService(header={"version" : version})
+        payload = await auth.verify_and_create_token( service, path, request, access_token, version )
+        return await auth.forward_api("DELETE", service, path, request, payload.get("sub"))
